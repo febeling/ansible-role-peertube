d6f449ae0aaf78e1412bb36b73b2e2ab47b79827
Alex Kotov <kotovalexarian@gmail.com>
2020-06-12 14:31:20 +0500

    Validate more variables

--- action_plugins/validate_vars.py
+++ action_plugins/validate_vars.py
@@ -7,10 +7,21 @@ class ActionModule(ActionBase):
     prefix = 'peertube'
 
     private_var_names = [
+        'archive_file',
+        'config_dir',
+        'config_file',
+        'current_link',
+        'dir',
         'group',
+        'npm',
         'service',
+        'storage_dir',
         'user',
+        'version_dir',
+        'versions_dir',
     ]
+
+    path_re = re.compile(r'^/[-_/a-z0-9]*$')
 
     common_name_re = re.compile(r'^[a-z_][a-z0-9_-]{0,30}(\$|[a-z0-9_-])?$')
 
@@ -48,10 +59,46 @@ class ActionModule(ActionBase):
 
         return result
 
+    def validate_archive_file(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_config_dir(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_config_file(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_current_link(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_dir(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
     def validate_group(self, value):
         if not isinstance(value, str):
             return 'is not str'
         if not self.common_name_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_npm(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
             return 'has invalid format'
 
     def validate_service(self, value):
@@ -60,8 +107,26 @@ class ActionModule(ActionBase):
         if not self.common_name_re.fullmatch(value):
             return 'has invalid format'
 
+    def validate_storage_dir(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
     def validate_user(self, value):
         if not isinstance(value, str):
             return 'is not str'
         if not self.common_name_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_version_dir(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
+            return 'has invalid format'
+
+    def validate_versions_dir(self, value):
+        if not isinstance(value, str):
+            return 'is not str'
+        if not self.path_re.fullmatch(value):
             return 'has invalid format'
